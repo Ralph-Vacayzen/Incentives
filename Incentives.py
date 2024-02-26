3146959f1df013a4da72af6b637479b49da3f1ad
Ralph McCracken <ralph@mccracken.work>
2024-02-26 12:43:03 -0600

    saving...

--- Incentives.py
+++ Incentives.py
@@ -44,51 +44,33 @@ if house_agreements is not None and dispatches is not None and prepayments is no
     dda = pd.read_csv(dispatches)
     dp  = pd.read_csv(prepayments)
 
-    dda['Dispatch'] = pd.to_datetime(dda['Dispatch']).dt.date
-    dda = dda[(dda.Dispatch >= start) & (dda.Dispatch <= end)]
-
 
     def IsBS(row):
-        return row.Product == 'Beach Services' or row.DeliverOrPickupToType == 'BEACH SERVICE SET UP' # TODO: Implement beach service logic
+        return (row.Product == 'Beach Services') or (row.DeliverOrPickupToType == 'BEACH SERVICE SET UP') # TODO: Implement beach service logic
 
     def IsLSV(row):
         return (row.Product == 'Golf Cart Rentals') or (row.DeliverOrPickupToType in settings['LSV']['DISPATCH']['SPECIFIC'])
 
     def IsB2B(row):
-        isNotBS  = not (row.isBS)
-        isNotLSV = not (row.isLSV)
-        isB2B    = row.RentalAgreementID in dha['RentalAgreementID'].to_list()
+        isNotBS           = not (row.isBS)
+        isNotLSV          = not (row.isLSV)
+        isOnHouseProgram  = row.RentalAgreementID in dha['RentalAgreementID'].to_list()
 
-        return isNotBS and isNotLSV and isB2B
+        return isNotBS and isNotLSV and isOnHouseProgram
     
     def IsB2C(row):
         return not (row.isBS or row.isLSV or row.isB2B)
     
-    def IsIgnore(row):
+    def IsIgnored(row):
         ignoreList = ['ABANDONED', 'MISC ERRAND']
 
         return row.DeliverOrPickupToType in ignoreList
-
-
-
-    dda['isBS']     = dda.apply(IsBS,  axis=1)
-    dda['isLSV']    = dda.apply(IsLSV, axis=1)
-    dda['isB2B']    = dda.apply(IsB2B, axis=1)
-    dda['isB2C']    = dda.apply(IsB2C, axis=1)
-    dda['isIgnore'] = dda.apply(IsIgnore, axis=1)
-
-    dda = dda[~dda['isIgnore']]
-    dda = dda.drop(columns=['isIgnore'])
-
-
-
+    
     timestamps = {
         'LSV': {},
         'B2B': {},
         'B2C': {}
     }
-
-
 
     def IdentifyTimestamps(row):
 
@@ -106,13 +88,66 @@ if house_agreements is not None and dispatches is not None and prepayments is no
                 timestamps[department][row.RentalAgreementID].add(row.Dispatch)
             else:
                 timestamps[department][row.RentalAgreementID].add(row.Dispatch)
+    
+    def IsRequiredWork(row):
+
+        department = None
+
+        if   row.isLSV: department = 'LSV'
+        elif row.isB2B: department = 'B2B'
+        elif row.isB2C: department = 'B2C'
+
+        if department == None: return False
+
+        return row.DeliverOrPickupToType in settings[department]['DISPATCH']['REQUIRED']
+
+    def IsAdditionalWork(row):
+
+        department = None
+
+        if   row.isLSV: department = 'LSV'
+        elif row.isB2B: department = 'B2B'
+        elif row.isB2C: department = 'B2C'
+
+        if department == None: return False
+
+        return row.DeliverOrPickupToType not in settings[department]['DISPATCH']['REQUIRED']
+    
+    def IsError(row):
+
+        department = None
+
+        if   row.isLSV: department = 'LSV'
+        elif row.isB2B: department = 'B2B'
+        elif row.isB2C: department = 'B2C'
+
+        if department == None: return False
+
+        if row.isAdditionalWork and row.RentalAgreementID in timestamps[department]:
+            if   row.Dispatch                             in timestamps[department][row.RentalAgreementID]: return True
+            elif row.Dispatch - pd.Timedelta(days=1)      in timestamps[department][row.RentalAgreementID]: return True
+            elif row.Dispatch - pd.Timedelta(days=2)      in timestamps[department][row.RentalAgreementID]: return True
+
+        return False
+    
 
 
+
+    dda['Dispatch']         = pd.to_datetime(dda['Dispatch']).dt.date
+    dda                     = dda[(dda.Dispatch >= start) & (dda.Dispatch <= end)]
+    dda['isBS']             = dda.apply(IsBS,      axis=1)
+    dda['isLSV']            = dda.apply(IsLSV,     axis=1)
+    dda['isB2B']            = dda.apply(IsB2B,     axis=1)
+    dda['isB2C']            = dda.apply(IsB2C,     axis=1)
+    dda['isIgnored']        = dda.apply(IsIgnored, axis=1)
+    dda                     = dda[~dda['isIgnored']]
 
     dda.apply(IdentifyTimestamps, axis=1)
 
-    timestamps
+    dda['isRequiredWork']   = dda.apply(IsRequiredWork,   axis=1)
+    dda['isAdditionalWork'] = dda.apply(IsAdditionalWork, axis=1)
+    dda['isError']          = dda.apply(IsError,          axis=1)
 
-    # def IdentifyAdditionalWork(row): TODO
 
-    st.dataframe(dda, use_container_width=True, hide_index=True)
+    with st.expander('Showing the work'):
+        st.dataframe(dda, use_container_width=True, hide_index=True)
