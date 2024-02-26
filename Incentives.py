4a4e0027e6090ff073fc6ad70c65304d7862c9d9
Ralph McCracken <ralph@mccracken.work>
2024-02-25 20:38:06 -0600

    saving...

--- Incentives.py
+++ Incentives.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
 st.set_page_config(page_title='Incentives', page_icon='💰', layout="wide", initial_sidebar_state="auto", menu_items=None)
 
 st.caption('VACAYZEN')
@@ -15,97 +16,59 @@ end   = r.date_input('End of Period', min_value=start)
 
 settings = pd.read_json('settings.json')
 
-tab_b2b, tab_b2c, tab_lsv, tab_sales = st.tabs(['B2B','B2C','LSV','SALES'])
+with st.expander('Files'):
 
-with tab_b2b:
-
-    l, r = st.columns(2)
-
-    house_agreements = l.file_uploader(
-        label='House Program Order Numbers',
-        type='CSV',
-        help='These order numbers will be the only orders considered in terms of activity in this analysis.')
-    
-    disptach_activities = r.file_uploader(
-        label='Dispatch Activities',
+    dispatches = st.file_uploader(
+        label='**Dispatch Activities**',
         type='CSV',
         help='These are from the integraRental database report, Incentives_Dispatches.')
+
+    prepayments = st.file_uploader(
+        label='**Prepayments**',
+        type='CSV',
+        help='These are from the integraRental database report, Incentives_Prepayments.')
     
+    house_agreements = st.file_uploader(
+        label='**House Program Order Numbers**',
+        type='CSV',
+        help='These order numbers will be the only orders considered in terms of activity in this analysis.')
 
-    if house_agreements is not None and disptach_activities is not None:
 
-        dha = pd.read_csv(house_agreements)
-        dda = pd.read_csv(disptach_activities)
+if house_agreements is not None and dispatches is not None and prepayments is not None:
 
-        dda['Dispatch'] = pd.to_datetime(dda['Dispatch']).dt.date
+    dha = pd.read_csv(house_agreements)
+    dda = pd.read_csv(dispatches)
+    dp  = pd.read_csv(prepayments)
 
-        with st.expander('**Analysis**'):
+    dda['Dispatch'] = pd.to_datetime(dda['Dispatch']).dt.date
+    dda = dda[(dda.Dispatch >= start) & (dda.Dispatch <= end)]
 
-            st.info('Required and irrelevant services are maintained in the settings area.')
 
-            required = st.multiselect(
-                label='Required Dispatches',
-                options=dda.DeliverOrPickupToType.unique(),
-                default=settings['B2B']['DISPATCH']['Required'])
-            
-            irrelevant = st.multiselect(
-                label='Irrelevant Dispatches',
-                options=dda.DeliverOrPickupToType.unique(),
-                default=settings['B2B']['DISPATCH']['Irrelevant'])
-            
-            remaining = (set(dda.DeliverOrPickupToType.unique()) - set(settings['B2B']['DISPATCH']['Required'])) - set(settings['B2B']['DISPATCH']['Irrelevant'])
-            
-            additional = st.multiselect(
-                label='Additional Work Dispatches',
-                options=dda.DeliverOrPickupToType.unique(),
-                default=remaining)
-            
-            st.info('Dispatches are isolated to the date range.')
-            st.info('Dispatches validated based on: house agreement, required dispatch, and additional dispatch.')
+    def IsBS(row):
+        return row.Product == 'Beach Services' or row.DeliverOrPickupToType == 'BEACH SERVICE SET UP'
 
-            dda = dda[(dda.Dispatch >= start) & (dda.Dispatch <= end)]
+    def IsLSV(row):
+        return row.Product == 'Golf Cart Rentals'
 
-            def IsHouseAgreement(row):
-                return row.RentalAgreementID in dha.RentalAgreementID
-            
-            def IsRequiredDispatch(row):
-                return row.DeliverOrPickupToType in required
-            
-            def IsAdditionalDispatch(row):
-                return row.DeliverOrPickupToType in additional
-            
-            dda['house']      = dda.apply(IsHouseAgreement,     axis=1)
-            dda['required']   = dda.apply(IsRequiredDispatch,   axis=1)
-            dda['additional'] = dda.apply(IsAdditionalDispatch, axis=1)
+    def IsB2B(row):
+        isNotBS  = not (row.isBS)
+        isNotLSV = not (row.isLSV)
+        isB2B    = row.RentalAgreementID in dha['RentalAgreementID'].to_list()
 
-            dda = dda[dda.house]
-            dda = dda[dda.required | dda.additional]
+        return isNotBS and isNotLSV and isB2B
+    
+    def IsB2C(row):
+        return not (row.isBS or row.isLSV or row.isB2B)
+    
+    def IsIgnore(row):
+        return row.DeliverOrPickupToType == 'ABANDONED' or row.DeliverOrPickupToType == 'MISC ERRAND'
 
-            st.dataframe(dda, use_container_width=True, hide_index=True)
+    dda['isBS']     = dda.apply(IsBS,  axis=1)
+    dda['isLSV']    = dda.apply(IsLSV, axis=1)
+    dda['isB2B']    = dda.apply(IsB2B, axis=1)
+    dda['isB2C']    = dda.apply(IsB2C, axis=1)
+    dda['isIgnore'] = dda.apply(IsIgnore, axis=1)
 
-            st.info('Dispatches are counted.')
+    print(dda.DeliverOrPickupToType.unique())
 
-        
-        count_required   = np.count_nonzero(dda[dda.required]['required'])
-        count_additional = np.count_nonzero(dda[dda.additional]['additional'])
-        efficiency       = 1 - (count_additional / count_required)
-
-        with st.container(border=True):
-
-            l, m, r = st.columns(3)
-
-            l.metric(
-                label='Required Dispatches',
-                value=count_required
-            )
-
-            m.metric(
-                label='Additional Dispatches',
-                value=count_additional
-            )
-
-            r.metric(
-                label='Efficiency',
-                value=round(efficiency, 4),
-                delta=round(efficiency - 0.97, 2)
-            )
+    st.dataframe(dda, use_container_width=True, hide_index=True)
