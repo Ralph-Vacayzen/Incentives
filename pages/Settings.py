e0905ccc71614e4cd3a6657fc44e0177ec458e7f
Ralph McCracken <ralph@mccracken.work>
2024-03-21 11:31:17 -0500

    login to get to settings

--- pages/Settings.py
+++ pages/Settings.py
@@ -11,28 +11,13 @@ st.caption('VACAYZEN')
 st.title('Settings')
 
 if not logged_in:
-    def Create_Stream(phrase):
-        for word in phrase.split(' '):
-            yield word + ' '
-            time.sleep(0.03)
-
-    messages = st.container(border=True)
-    messages.chat_message('assistant').write_stream(Create_Stream('Good day! I am the **Pass Ward**. Please provide the phrase to pass through this security check.'))
-
-    if prompt := st.chat_input():
-        messages.chat_message('user').write_stream(Create_Stream(prompt))
-
-        if prompt == st.secrets['SETTINGS_PASSWORD']:
-            logged_in == True
-        else:
-            messages.chat_message('assistant').write_stream(Create_Stream('That did not seem to match my records. Are you sure you should be here? Please try another passphrase.'))
-
-
-
-
-        
-        
-
+    container = st.empty()
+    prompt = container.text_input('Please enter password to proceed:', type='password',)
+    if prompt == st.secrets['SETTINGS_PASSWORD']:
+        logged_in = True
+        container.empty()
+    elif prompt != '' and prompt != st.secrets['SETTINGS_PASSWORD']:
+        st.error('Password incorrect.')
 
 if logged_in:
     st.info('Be sure to save any changes after each adjustment, and **double-check the save went through.**')
